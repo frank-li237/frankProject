8918f3a6f3a4da6ab98bffacec3e007473c34e74
frank-Li237 <FrankLi@poweronlabs.com>
2024-08-12 20:41:41 +0800

    feat：2

--- cocos-js/cc.4b9e1.js
+++ cocos-js/cc.4b9e1.js
@@ -1,4 +1,4 @@
-System.register(['./virtual_cc-bf54316d.js'], (function (exports) {
+System.register(['./_virtual_cc-bf54316d.js'], (function (exports) {
 	'use strict';
 	return {
 		setters: [function (module) {
